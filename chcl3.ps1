d15e41dd1f207ee61e5c40d4291294355d343f88
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-05-16 09:54:07 +0800

    Add PowerShell basic skeleton

---
+++ chcl3.ps1
@@ -0,0 +1,14 @@
+﻿[bool]$DEBUG = $false
+
+Function Main {
+    return 0
+}
+
+Function CleanUp {
+    Write-Host "Cleaning up..."
+}
+
+[int]$ExitCode = Main
+CleanUp
+#$Host.SetShouldExit($ExitCode)
+#Exit $ExitCode
