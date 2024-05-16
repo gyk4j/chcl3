f3728138d5e86ed14e62e0872c1caa486dd78ddd
gyk <147011991+gyk4j@users.noreply.github.com>
2024-05-16 11:06:00 +0800

    Add display warning (#34)

--- chcl3.ps1
+++ chcl3.ps1
@@ -16,8 +16,31 @@ Function Read-TextFile {
 Function Display-License {
     [string]$License = Read-TextFile -Path "LICENSE"
     $License = $License.Replace($CRLF + $CRLF, "~~~").Replace($CRLF, "").Replace("~~~", $CRLF + $CRLF)
+    $License = $License + $CRLF + $CRLF + "Click Cancel to exit now or OK to accept the license." + $CRLF
 
     return [System.Windows.MessageBox]::Show($License, "License Agreement", "OKCancel", "Information")
+}
+
+Function Display-Warning {
+    [string]$Warning = @"
+This script disables and/or removes log data, Windows 
+services, registry keys, scheduled tasks Windows updates associated 
+with Windows Telemetry. 
+It also blocks IPs and domain names to prevent the uploading of 
+collected telemetry data.
+
+WARNING: These changes may cause operating system damage or 
+impairments!
+
+By proceeding with the use of this script, you accept the license, 
+terms and conditions and all associated risks.
+
+Click Cancel to exit now or OK to continue.
+"@
+
+    $Warning = $Warning.Replace($CRLF + $CRLF, "~~~").Replace($CRLF, "").Replace("~~~", $CRLF + $CRLF)
+
+    return [System.Windows.MessageBox]::Show($Warning, "Warning", "OKCancel", "Exclamation")
 }
 
 Function Main {
@@ -25,7 +48,12 @@ Function Main {
 
     $Accepted = Display-License    
     if ($Accepted -ne "OK") {
-        return -1
+        return 1
+    }
+
+    $Accepted = Display-Warning    
+    if ($Accepted -ne "OK") {
+        return 2
     }
     
     return 0
