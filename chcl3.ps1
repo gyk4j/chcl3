a777eaaf20fcfa31ed8e236d1b2a0e89bfdcc3e3
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-05-16 10:44:10 +0800

    Add display license

--- chcl3.ps1
+++ chcl3.ps1
@@ -1,14 +1,42 @@
-﻿[bool]$DEBUG = $false
+﻿Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+[string]$CRLF = "`r`n"
+[bool]$DEBUG = $false
+
+Function Change-ScriptDirectory {
+    Set-Location -Path $PSScriptRoot
+}
+
+Function Read-TextFile {
+    Param ( [string]$Path )
+
+    return Get-Content -Path $Path -Raw
+}
+
+Function Display-License {
+    [string]$License = Read-TextFile -Path "LICENSE"
+    $License = $License.Replace($CRLF + $CRLF, "~~~").Replace($CRLF, "").Replace("~~~", $CRLF + $CRLF)
+
+    return [System.Windows.MessageBox]::Show($License, "License Agreement", "OKCancel", "Information")
+}
 
 Function Main {
+    Change-ScriptDirectory
+
+    $Accepted = Display-License    
+    if ($Accepted -ne "OK") {
+        return -1
+    }
+    
     return 0
 }
 
 Function CleanUp {
-    Write-Host "Cleaning up..."
+    #Write-Host "Cleaning up..."
 }
 
 [int]$ExitCode = Main
 CleanUp
+$ExitCode
 #$Host.SetShouldExit($ExitCode)
 #Exit $ExitCode
